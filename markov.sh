271be68ee0c8f416c0c1e42f3d65fb5f903d9040
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-02 16:01:23 +0200

    Update markov.sh

--- markov.sh
+++ markov.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
+if [ $(echo "\e[0m") != $(echo -e "\e[0m") ]; then
+    DEF="\e[0m"; RED="\e[31m"; GREEN="\e[32m"; BLUE="\e[34m"; PURP="\e[35m"
+fi
+
+
+
 if [ $# -eq 0 ]; then
-    echo -e "\e[31mError type 1:\e[0m no algorithm(s) passed.
-       \e[35mUsage:\e[0m $0 <algorithm 1> [algorithm 2] ..." >&2
+    echo -e "${RED}Error type 1:${DEF} no algorithm(s) passed.
+       ${PURP}Usage:${DEF} $0 [options] <algorithm 1> [algorithm 2] ..." >&2
     exit 2
 fi
 
 
 
-
-missing_algorithms=( "missing algorithm(s)" )
 for passed_algorithm in "$@"; do
     if ! [ -f "$passed_algorithm" ]; then
         missing_algorithms+=("$passed_algorithm,")
     fi
 done
 
-if [ ${#missing_algorithms[@]} -ne 1 ]; then
-    echo -e "\e[31mError type 2:\e[0m ${missing_algorithms[@]}
-       \e[35mUsage:\e[0m $0 <algorithm 1> [algorithm 2] ..." | sed 's/,$/./' >&2
+if [ ${#missing_algorithms[@]} -ne 0 ]; then
+    echo -e "${RED}Error type 2:${DEF} missing algorithm(s) ${missing_algorithms[@]}
+       ${PURP}Usage:${DEF} $0 [options] <algorithm 1> [algorithm 2] ..." | sed 's/,$/./' >&2
     exit 2
 fi
-
 
 
 
@@ -29,9 +32,18 @@ while true; do
     read -e -p "Word processed: " WORD
     ALNUM_WORD=$(echo "$WORD" | tr -cd [:alnum:])
     if [ "$WORD" != "$ALNUM_WORD" ]; then
-        echo -e -n "\e[34mWarning type 1:\e[0m \"$WORD\" includes non alpha-numerical characters.
-        \e[32mAction:\e[0m re-enter the word [Y] or proceed with \"$ALNUM_WORD\" [n]? "; read;
+        echo -e -n "${BLUE}Warning type 1:${DEF} \"$WORD\" includes non alpha-numerical characters.
+        ${GREEN}Action:${DEF} re-enter the word [Y] or proceed with \"$ALNUM_WORD\" [n]? "; read;
         if [[ $REPLY =~ ^[nNmMbBтТьЬиИ] ]]; then echo "Word processed: $ALNUM_WORD"; break; fi
     else break
     fi
+done
+
+
+
+for algorithm in "$@"; do
+    ###
+    while true; do
+        ###
+    done
 done
